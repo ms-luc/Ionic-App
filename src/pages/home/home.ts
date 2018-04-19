28fd33db680c01100a367a5e8ab75ea2317f167e
ms-luc <mishashan32@gmail.com>
2018-04-19 17:43:03 -0400

    update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,23 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+ 
+  items: any[];
 
   constructor(public navCtrl: NavController) {
+    this.items = [];
+    //populate a list of items with dummy records
+    for(let i = 0; i < 10; i++){
+      this.items.push({
+        text: 'Item' + i,
+        id: i
+      })
+    }
 
+  }
+
+  itemSelected(item){
+    alert('This is ' + item.text);
   }
 
 }
